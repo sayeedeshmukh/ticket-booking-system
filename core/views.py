b0388c5feb99a80b99df384b69a0cb549fbed810
Sayee Deshmukh <sayyeee21@gmail.com>
2025-04-25 12:19:04 +0530

    added manual user registration, login and logout

--- core/views.py
+++ core/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')
+    
+    def post(self, request):
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        User.objects.create_user(username=username, email=email, password=password)
+        return redirect('login')
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+    
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        return render(request, 'login.html', {'error': 'Invalid credentials'})
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
